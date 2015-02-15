4fd68b07acc6b8b085055cd36ab6de3f9722d260
Marina Murashev <marina@hoteltonight.com>
2015-02-15 13:32:26 -0800

    applies viewholder pattern to grid view adapter

--- app/src/main/java/com/example/marinamurashev/gridimagesearch/adapters/ImageResultsAdapter.java
+++ app/src/main/java/com/example/marinamurashev/gridimagesearch/adapters/ImageResultsAdapter.java
@@ -16,7 +16,12 @@ import com.squareup.picasso.Picasso;
 import java.util.List;
 
 public class ImageResultsAdapter extends ArrayAdapter<ImageResult> {
-
+    
+    private static class ViewHolder {
+        TextView tvTitle;
+        ImageView ivImage;
+    }
+    
     public ImageResultsAdapter(Context context, List<ImageResult> images) {
         super(context, R.layout.item_image_result, images);
     }
@@ -24,16 +29,23 @@ public class ImageResultsAdapter extends ArrayAdapter<ImageResult> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageResult imageResult = getItem(position);
+
+        ViewHolder viewHolder;
+        
         if(convertView == null){
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_image_result, parent, false);
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_image_result, parent, false);
+            viewHolder.ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
+            viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
         
-        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
-        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-        
-        ivImage.setImageResource(0);
-        tvTitle.setText(Html.fromHtml(imageResult.getTitle()));
-        Picasso.with(getContext()).load(imageResult.getThumbUrl()).into(ivImage);
+        viewHolder.ivImage.setImageResource(0);
+        viewHolder.tvTitle.setText(Html.fromHtml(imageResult.getTitle()));
+        Picasso.with(getContext()).load(imageResult.getThumbUrl()).into(viewHolder.ivImage);
         
         return convertView;
     }
